474bd4504a97eea6a10d9cc9633c864783f1abad
go shibata <warahiko@gmail.com>
2021-05-19 17:04:11 +0900

    HomeFragment 追加

---
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeFragment.kt
@@ -0,0 +1,20 @@
+package io.github.warahiko.shoppingmemoapp.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            HomeScreen()
+        }
+    }
+}
