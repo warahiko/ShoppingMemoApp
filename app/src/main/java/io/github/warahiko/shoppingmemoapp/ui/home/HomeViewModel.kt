bed04386d9b7e988d82ef7576113372a2e8b0e82
go shibata <warahiko@gmail.com>
2021-05-20 12:52:17 +0900

    ViewModel でUseCase を使うよう更新

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeViewModel.kt
@@ -3,8 +3,8 @@ package io.github.warahiko.shoppingmemoapp.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.github.warahiko.shoppingmemoapp.data.repository.ShoppingListRepository
 import io.github.warahiko.shoppingmemoapp.model.ShoppingItem
+import io.github.warahiko.shoppingmemoapp.usecase.FetchShoppingListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
@@ -13,16 +13,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val shoppingListRepository: ShoppingListRepository,
+    private val fetchShoppingListUseCase: FetchShoppingListUseCase,
 ) : ViewModel() {
 
     private val _shoppingListFlow = MutableStateFlow<List<ShoppingItem>>(listOf())
     val shoppingListFlow: StateFlow<List<ShoppingItem>> = _shoppingListFlow
 
     fun fetchShoppingList() = viewModelScope.launch {
-        shoppingListRepository.getShoppingList()
-            .collect {
-                _shoppingListFlow.value = it
-            }
+        fetchShoppingListUseCase().collect {
+            _shoppingListFlow.value = it
+        }
     }
 }
