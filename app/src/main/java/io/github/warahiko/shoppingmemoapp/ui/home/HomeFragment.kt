cf19b15606fed9996a68ba39fd740a7d9652c61c
go shibata <warahiko@gmail.com>
2021-05-20 10:57:57 +0900

    HomeFragment でrepository をinject

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeFragment.kt
@@ -7,22 +7,22 @@ import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.warahiko.shoppingmemoapp.data.repository.ShoppingListRepository
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    @Inject
+    lateinit var shoppingListRepository: ShoppingListRepository
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View = ComposeView(requireContext()).apply {
         setContent {
-            // TODO: 仮置き
-            val items = listOf(
-                ShoppingItem(id = UUID.randomUUID(), name = "にんじん", 1),
-                ShoppingItem(id = UUID.randomUUID(), name = "たまねぎ", 1),
-                ShoppingItem(id = UUID.randomUUID(), name = "卵", 1),
-                ShoppingItem(id = UUID.randomUUID(), name = "牛乳", 3),
-            )
+            val items = shoppingListRepository.getShoppingList()
             HomeScreen(items)
         }
     }
