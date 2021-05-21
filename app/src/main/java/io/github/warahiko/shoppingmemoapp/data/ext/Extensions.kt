8877fc22bb49635ed4522763d59705a8c32b5824
go shibata <warahiko@gmail.com>
2021-05-21 15:36:54 +0900

    String からrichText のリストに変換する拡張関数追加

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/ext/Extensions.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/ext/Extensions.kt
@@ -1,7 +1,10 @@
 package io.github.warahiko.shoppingmemoapp.data.ext
 
 import io.github.warahiko.shoppingmemoapp.data.network.model.RichText
+import io.github.warahiko.shoppingmemoapp.data.network.model.Text
 
 fun List<RichText>.concatText() = fold("") { acc, richText ->
     acc + richText.text.content
 }
+
+fun String.toRichTextList(): List<RichText> = listOf(RichText(Text(this)))
