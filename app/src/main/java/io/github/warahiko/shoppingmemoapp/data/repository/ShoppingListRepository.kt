236da18182249cfc2ba83b2de0b037e602b93348
go shibata <warahiko@gmail.com>
2021-05-22 13:26:12 +0900

    アイテム追加用API を叩く関数を ShoppingListRepository に追加

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/ShoppingListRepository.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/ShoppingListRepository.kt
@@ -6,4 +6,6 @@ import kotlinx.coroutines.flow.Flow
 interface ShoppingListRepository {
 
     suspend fun getShoppingList(): Flow<List<ShoppingItem>>
+
+    suspend fun addShoppingItem(shoppingItem: ShoppingItem): Flow<Boolean>
 }
--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/impl/ShoppingListRepositoryImpl.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/impl/ShoppingListRepositoryImpl.kt
@@ -1,7 +1,11 @@
 package io.github.warahiko.shoppingmemoapp.data.repository.impl
 
 import io.github.warahiko.shoppingmemoapp.BuildConfig
+import io.github.warahiko.shoppingmemoapp.data.ext.toRichTextList
 import io.github.warahiko.shoppingmemoapp.data.network.api.ShoppingListApi
+import io.github.warahiko.shoppingmemoapp.data.network.model.AddShoppingItemRequest
+import io.github.warahiko.shoppingmemoapp.data.network.model.Database
+import io.github.warahiko.shoppingmemoapp.data.network.model.Property
 import io.github.warahiko.shoppingmemoapp.data.repository.ShoppingListRepository
 import io.github.warahiko.shoppingmemoapp.model.ShoppingItem
 import kotlinx.coroutines.Dispatchers
@@ -29,5 +33,17 @@ class ShoppingListRepositoryImpl @Inject constructor(
             )
         }
         emit(items)
+    }.flowOn(Dispatchers.IO)
+
+    override suspend fun addShoppingItem(shoppingItem: ShoppingItem): Flow<Boolean> = flow {
+        val requestBody = mapOf(
+            "Name" to Property(title = shoppingItem.name.toRichTextList()),
+            "Count" to Property(number = shoppingItem.count.toLong()),
+            "IsDone" to Property(isChecked = shoppingItem.isDone),
+            "Memo" to Property(richText = shoppingItem.memo.toRichTextList()),
+        )
+        val request = AddShoppingItemRequest(Database(BuildConfig.DATABASE_ID), requestBody)
+        val response = shoppingListApi.addShoppingItem(request)
+        emit(response.isSuccessful)
     }.flowOn(Dispatchers.IO)
 }
