5fd7d1991d54e17dfe4f6b6ac03f9a2433a927cf
go shibata <warahiko@gmail.com>
2021-05-22 13:51:12 +0900

    ShoppingMemoScaffold にmodifier パラメータ追加

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/ShoppingMemoScaffold.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/ShoppingMemoScaffold.kt
@@ -10,12 +10,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
 
 @Composable
 fun ShoppingMemoScaffold(
+    modifier: Modifier = Modifier,
     title: String = "",
     appBarIcon: ImageVector? = null,
     floatingActionButton: @Composable () -> Unit = {},
@@ -23,6 +25,7 @@ fun ShoppingMemoScaffold(
 ) {
     ShoppingMemoAppTheme {
         Scaffold(
+            modifier = modifier,
             topBar = {
                 ShoppingMemoAppBar(title = title, icon = appBarIcon)
             },
