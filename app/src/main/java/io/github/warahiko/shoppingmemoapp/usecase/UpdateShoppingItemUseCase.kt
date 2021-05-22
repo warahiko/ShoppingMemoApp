5b60e1ff4c1919f06d527db64e4bda583832ca43
go shibata <warahiko@gmail.com>
2021-05-22 14:55:33 +0900

    アイテム更新用UseCase 追加

---
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/usecase/UpdateShoppingItemUseCase.kt
@@ -0,0 +1,8 @@
+package io.github.warahiko.shoppingmemoapp.usecase
+
+import io.github.warahiko.shoppingmemoapp.model.ShoppingItem
+import kotlinx.coroutines.flow.Flow
+
+interface UpdateShoppingItemUseCase {
+    suspend operator fun invoke(shoppingItem: ShoppingItem): Flow<Boolean>
+}
