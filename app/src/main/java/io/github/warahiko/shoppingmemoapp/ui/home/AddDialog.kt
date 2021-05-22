c67f5328d0239acb44ba0f1ecf8406df3427cfbc
go shibata <warahiko@gmail.com>
2021-05-22 13:22:21 +0900

    AlertDialog でTextField が使いづらいのでDialog に変更 Title をなくしてButton に文字列リソース移動

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/AddDialog.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/AddDialog.kt
@@ -3,17 +3,20 @@ package io.github.warahiko.shoppingmemoapp.ui.home
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.AlertDialog
+import androidx.compose.material.Button
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import io.github.warahiko.shoppingmemoapp.R
 import io.github.warahiko.shoppingmemoapp.model.ShoppingItem
 import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
@@ -34,19 +37,24 @@ fun AddDialog(
             "",
         ))
     }
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = {
-            Text(stringResource(R.string.home_add_dialog_title))
-        },
-        text = {
-            AddDialogContent(shoppingItem = shoppingItem, onChangeItem = setShoppingItem)
-        },
-        confirmButton = {
-            onAdd(shoppingItem)
-        },
-        modifier = modifier,
-    )
+    Dialog(onDismissRequest = onDismiss) {
+        Surface {
+            Column(modifier = modifier) {
+                AddDialogContent(
+                    shoppingItem = shoppingItem,
+                    onChangeItem = setShoppingItem,
+                )
+                Button(
+                    onClick = { onAdd(shoppingItem) },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.End),
+                ) {
+                    Text(stringResource(R.string.home_add_dialog_button))
+                }
+            }
+        }
+    }
 }
 
 @Composable
