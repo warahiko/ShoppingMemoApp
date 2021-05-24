70edc27a336c8b8bb2038f03447c30786fb28333
go shibata <warahiko@gmail.com>
2021-05-24 16:27:57 +0900

    GetShoppingList　API でフィルタを適用できるよう更新

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/network/api/ShoppingListApi.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/network/api/ShoppingListApi.kt
@@ -1,6 +1,7 @@
 package io.github.warahiko.shoppingmemoapp.data.network.api
 
 import io.github.warahiko.shoppingmemoapp.data.network.model.AddShoppingItemRequest
+import io.github.warahiko.shoppingmemoapp.data.network.model.GetShoppingListRequest
 import io.github.warahiko.shoppingmemoapp.data.network.model.Page
 import io.github.warahiko.shoppingmemoapp.data.network.model.ShoppingListResponse
 import io.github.warahiko.shoppingmemoapp.data.network.model.UpdateItemRequest
@@ -12,7 +13,10 @@ import retrofit2.http.Path
 interface ShoppingListApi {
 
     @POST("databases/{database_id}/query")
-    suspend fun getShoppingList(@Path("database_id") databaseId: String): ShoppingListResponse
+    suspend fun getShoppingList(
+        @Path("database_id") databaseId: String,
+        @Body request: GetShoppingListRequest,
+    ): ShoppingListResponse
 
     @POST("pages")
     suspend fun addShoppingItem(@Body request: AddShoppingItemRequest): Page
