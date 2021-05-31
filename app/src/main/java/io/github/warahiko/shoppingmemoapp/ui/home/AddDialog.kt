9752ab5f2e2f744e5cae3784345118014a9276b7
go shibata <warahiko@gmail.com>
2021-05-31 15:52:32 +0900

    アイテム内容設定部分をファイル切り出し、リネーム

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/AddDialog.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/AddDialog.kt
@@ -2,25 +2,19 @@ package io.github.warahiko.shoppingmemoapp.ui.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import io.github.warahiko.shoppingmemoapp.R
 import io.github.warahiko.shoppingmemoapp.model.ShoppingItem
-import io.github.warahiko.shoppingmemoapp.preview.getSample
-import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
 
 @Composable
 fun AddDialog(
@@ -32,7 +26,7 @@ fun AddDialog(
     Dialog(onDismissRequest = onDismiss) {
         Surface {
             Column(modifier = modifier) {
-                AddDialogContent(
+                EditingShoppingItemContent(
                     shoppingItem = shoppingItem,
                     onChangeItem = setShoppingItem,
                 )
@@ -46,56 +40,5 @@ fun AddDialog(
                 }
             }
         }
-    }
-}
-
-@Composable
-fun AddDialogContent(
-    shoppingItem: ShoppingItem,
-    onChangeItem: (ShoppingItem) -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    Column(modifier = modifier) {
-        TextField(
-            value = shoppingItem.name,
-            onValueChange = {
-                onChangeItem(shoppingItem.copy(name = it))
-            },
-            label = {
-                Text(stringResource(R.string.home_add_dialog_name))
-            },
-            singleLine = true,
-            modifier = Modifier.padding(8.dp),
-        )
-        TextField(
-            value = shoppingItem.count.toString(),
-            onValueChange = {
-                onChangeItem(shoppingItem.copy(count = it.toInt()))
-            },
-            label = {
-                Text(stringResource(R.string.home_add_dialog_count))
-            },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.padding(8.dp),
-        )
-        TextField(
-            value = shoppingItem.memo,
-            onValueChange = {
-                onChangeItem(shoppingItem.copy(memo = it))
-            },
-            label = {
-                Text(stringResource(R.string.home_add_dialog_memo))
-            },
-            modifier = Modifier.padding(8.dp),
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun AddDialogContentPreview() {
-    val item = ShoppingItem.getSample()
-    ShoppingMemoAppTheme {
-        AddDialogContent(item, {})
     }
 }
