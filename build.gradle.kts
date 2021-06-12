c09971daff565fc4c830f9a7ecd969453b55a952
go shibata <warahiko@gmail.com>
2021-06-13 01:44:31 +0900

    依存更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,14 @@
 buildscript {
-    val compose_version by extra("1.0.0-beta07")
+    val compose_version by extra("1.0.0-beta08")
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta02")
-        @Suppress("GradleDependency") // compose と 1.5.0 が合わない
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.4.32")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.31-alpha")
+        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.5.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.37")
     }
 }
 
