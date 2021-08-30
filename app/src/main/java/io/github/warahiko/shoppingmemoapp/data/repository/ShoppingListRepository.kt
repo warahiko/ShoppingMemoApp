e0959e4f8089a09680b3deb320b24dfec13e373f
go shibata <warahiko@gmail.com>
2021-08-30 23:38:53 +0900

    ShoppingListRepository にcache を置く、ShoppingList 取得関数の返値の型変更

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/ShoppingListRepository.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/repository/ShoppingListRepository.kt
@@ -5,18 +5,23 @@ import io.github.warahiko.shoppingmemoapp.data.mapper.relations
 import io.github.warahiko.shoppingmemoapp.data.mapper.toProperties
 import io.github.warahiko.shoppingmemoapp.data.mapper.toShoppingItem
 import io.github.warahiko.shoppingmemoapp.data.model.ShoppingItem
+import io.github.warahiko.shoppingmemoapp.data.model.Status
 import io.github.warahiko.shoppingmemoapp.data.network.api.ShoppingListApi
 import io.github.warahiko.shoppingmemoapp.data.network.model.AddShoppingItemRequest
 import io.github.warahiko.shoppingmemoapp.data.network.model.Database
 import io.github.warahiko.shoppingmemoapp.data.network.model.Filter
+import io.github.warahiko.shoppingmemoapp.data.network.model.FilterProperty
+import io.github.warahiko.shoppingmemoapp.data.network.model.FilterSelect
 import io.github.warahiko.shoppingmemoapp.data.network.model.GetShoppingListRequest
 import io.github.warahiko.shoppingmemoapp.data.network.model.UpdateItemRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -25,36 +30,67 @@ class ShoppingListRepository @Inject constructor(
     private val shoppingListApi: ShoppingListApi,
     private val tagListRepository: TagListRepository,
 ) {
+    private val _shoppingList: MutableStateFlow<List<ShoppingItem>?> = MutableStateFlow(null)
+    val shoppingList: StateFlow<List<ShoppingItem>?> = _shoppingList
 
-    suspend fun getShoppingList(filter: Filter?): Flow<List<ShoppingItem>> = flow {
+    suspend fun getOrFetchShoppingList(): List<ShoppingItem> {
+        return _shoppingList.value ?: fetchShoppingList()
+    }
+
+    suspend fun fetchShoppingList(): List<ShoppingItem> {
         val request = GetShoppingListRequest(filter = filter)
-        coroutineScope {
+        val (shoppingList, tagList) = withContext(Dispatchers.IO) {
             val shoppingListAsync = async {
                 shoppingListApi.getShoppingList(BuildConfig.DATABASE_ID, request)
             }
             val tagListAsync = async { tagListRepository.getOrFetchTagList() }
-            val shoppingList = shoppingListAsync.await()
-            val tagList = tagListAsync.await()
-            val items = shoppingList.results.map { item ->
-                val relationId = item.relations.first().id
-                val tag = tagList.single { it.id.toString() == relationId }
-                item.toShoppingItem().copy(tag = tag)
-            }
-            emit(items)
+            shoppingListAsync.await() to tagListAsync.await()
         }
-    }.flowOn(Dispatchers.IO)
+        return shoppingList.results.map { item ->
+            val relationId = item.relations.first().id
+            val tag = tagList.single { it.id.toString() == relationId }
+            item.toShoppingItem().copy(tag = tag)
+        }.also {
+            _shoppingList.value = it
+        }
+    }
 
     suspend fun addShoppingItem(shoppingItem: ShoppingItem): Flow<ShoppingItem> = flow {
         val requestBody = shoppingItem.toProperties()
         val request = AddShoppingItemRequest(Database(BuildConfig.DATABASE_ID), requestBody)
         val response = shoppingListApi.addShoppingItem(request)
-        emit(response.toShoppingItem())
+        val item = response.toShoppingItem()
+        _shoppingList.value = _shoppingList.value?.plus(item) ?: listOf(item)
+        emit(item)
     }.flowOn(Dispatchers.IO)
 
     suspend fun updateShoppingItem(shoppingItem: ShoppingItem): Flow<ShoppingItem> = flow {
         val properties = shoppingItem.toProperties()
         val request = UpdateItemRequest(properties)
         val response = shoppingListApi.updateShoppingItem(shoppingItem.id.toString(), request)
-        emit(response.toShoppingItem())
+        val item = response.toShoppingItem()
+        _shoppingList.value = _shoppingList.value
+            ?.map {
+                if (it.id == item.id) item else it
+            }
+            // TODO: filtering はViewModel 側で行うようにする
+            ?.filter {
+                it.status in listOf(Status.NEW, Status.DONE)
+            }
+        emit(item)
     }.flowOn(Dispatchers.IO)
+
+    // TODO: filtering はViewModel 側で行うようにする
+    private val filter = Filter(
+        or = listOf(
+            FilterProperty(
+                property = "Status",
+                select = FilterSelect(equals = Status.NEW.text),
+            ),
+            FilterProperty(
+                property = "Status",
+                select = FilterSelect(equals = Status.DONE.text),
+            ),
+        ),
+    )
 }
--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/HomeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.github.warahiko.shoppingmemoapp.data.model.ShoppingItem
 import io.github.warahiko.shoppingmemoapp.data.model.Tag
+import io.github.warahiko.shoppingmemoapp.data.repository.ShoppingListRepository
 import io.github.warahiko.shoppingmemoapp.data.repository.TagListRepository
 import io.github.warahiko.shoppingmemoapp.error.LaunchSafe
 import io.github.warahiko.shoppingmemoapp.usecase.AddShoppingItemUseCase
@@ -12,7 +13,6 @@ import io.github.warahiko.shoppingmemoapp.usecase.ArchiveShoppingItemUseCase
 import io.github.warahiko.shoppingmemoapp.usecase.ChangeShoppingItemIsDoneUseCase
 import io.github.warahiko.shoppingmemoapp.usecase.DeleteShoppingItemUseCase
 import io.github.warahiko.shoppingmemoapp.usecase.EditShoppingItemUseCase
-import io.github.warahiko.shoppingmemoapp.usecase.FetchShoppingListUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -24,7 +24,7 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     tagListRepository: TagListRepository,
-    private val fetchShoppingListUseCase: FetchShoppingListUseCase,
+    private val shoppingListRepository: ShoppingListRepository,
     private val addShoppingItemUseCase: AddShoppingItemUseCase,
     private val changeShoppingItemIsDoneUseCase: ChangeShoppingItemIsDoneUseCase,
     private val editShoppingItemUseCase: EditShoppingItemUseCase,
@@ -33,13 +33,10 @@ class HomeViewModel @Inject constructor(
     launchSafe: LaunchSafe,
 ) : ViewModel(), LaunchSafe by launchSafe {
 
-    private val _shoppingListFlow = MutableStateFlow<List<ShoppingItem>>(listOf())
-    val shoppingListFlow: StateFlow<List<ShoppingItem>>
-        get() = _shoppingListFlow
-            .map { list ->
-                list.sortedBy { it.name }
-            }
-            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), listOf())
+    val shoppingListFlow: StateFlow<List<ShoppingItem>> =
+        shoppingListRepository.shoppingList.map { list ->
+            list?.sortedBy { it.name } ?: emptyList()
+        }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(), listOf())
 
     val tagListFlow: StateFlow<List<Tag>> = tagListRepository.tagList.map {
         it ?: emptyList()
@@ -51,51 +48,28 @@ class HomeViewModel @Inject constructor(
 
     fun fetchShoppingList() = viewModelScope.launchSafe {
         _isRefreshing.value = true
-        fetchShoppingListUseCase().collect {
-            _shoppingListFlow.value = it
-            _isRefreshing.value = false
-        }
+        shoppingListRepository.fetchShoppingList()
+        _isRefreshing.value = false
     }
 
     fun addShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launchSafe {
-        addShoppingItemUseCase(shoppingItem)
-            .collect { resultItem ->
-                _shoppingListFlow.value = _shoppingListFlow.value + resultItem
-            }
+        addShoppingItemUseCase(shoppingItem).collect()
     }
 
     fun changeShoppingItemIsDone(shoppingItem: ShoppingItem, newIsDone: Boolean) =
         viewModelScope.launchSafe {
-            changeShoppingItemIsDoneUseCase(shoppingItem, newIsDone).collect { resultItem ->
-                _shoppingListFlow.value = _shoppingListFlow.value
-                    .toMutableList()
-                    .map {
-                        if (it.id == resultItem.id) resultItem else it
-                    }
-            }
+            changeShoppingItemIsDoneUseCase(shoppingItem, newIsDone).collect()
         }
 
     fun editShoppingItem(newShoppingItem: ShoppingItem) = viewModelScope.launchSafe {
-        editShoppingItemUseCase(newShoppingItem).collect { resultItem ->
-            _shoppingListFlow.value = _shoppingListFlow.value
-                .toMutableList()
-                .map {
-                    if (it.id == resultItem.id) resultItem else it
-                }
-        }
+        editShoppingItemUseCase(newShoppingItem).collect()
     }
 
     fun archiveShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launchSafe {
-        archiveShoppingItemUseCase(shoppingItem).collect { resultItem ->
-            _shoppingListFlow.value = _shoppingListFlow.value
-                .filter { it.id != resultItem.id }
-        }
+        archiveShoppingItemUseCase(shoppingItem).collect()
     }
 
     fun deleteShoppingItem(shoppingItem: ShoppingItem) = viewModelScope.launchSafe {
-        deleteShoppingItemUseCase(shoppingItem).collect { resultItem ->
-            _shoppingListFlow.value = _shoppingListFlow.value
-                .filter { it.id != resultItem.id }
-        }
+        deleteShoppingItemUseCase(shoppingItem).collect()
     }
 }
