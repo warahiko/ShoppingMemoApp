e40889653ef6925e473bc9ce98d640bf3ef61d89
go shibata <warahiko@gmail.com>
2021-09-05 23:50:24 +0900

    ShoppingItem のProperty クラスリネーム、Tag のnotion 上のプロパティをenum で定義

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/data/mapper/ShoppingItemMapper.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/data/mapper/ShoppingItemMapper.kt
@@ -4,47 +4,48 @@ import io.github.warahiko.shoppingmemoapp.data.ext.concatText
 import io.github.warahiko.shoppingmemoapp.data.model.ShoppingItem
 import io.github.warahiko.shoppingmemoapp.data.model.Status
 import io.github.warahiko.shoppingmemoapp.data.network.model.Date
+import io.github.warahiko.shoppingmemoapp.data.network.model.Property
 import io.github.warahiko.shoppingmemoapp.data.network.model.Relation
 import io.github.warahiko.shoppingmemoapp.data.network.model.Select
 import io.github.warahiko.shoppingmemoapp.data.network.model.ShoppingItemPage
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.UUID
-import io.github.warahiko.shoppingmemoapp.data.network.model.Property as PropertyModel
 
 fun ShoppingItemPage.toShoppingItem(): ShoppingItem {
     return ShoppingItem(
         id = UUID.fromString(id),
-        name = checkNotNull(properties[Property.Name.key]?.title?.concatText()),
-        count = checkNotNull(properties[Property.Count.key]?.number?.toInt()),
-        status = checkNotNull(properties[Property.Status.key]?.select?.let {
+        name = checkNotNull(properties[ShoppingItemProperty.Name.key]?.title?.concatText()),
+        count = checkNotNull(properties[ShoppingItemProperty.Count.key]?.number?.toInt()),
+        status = checkNotNull(properties[ShoppingItemProperty.Status.key]?.select?.let {
             Status.from(it.name)
         }),
-        doneDate = properties[Property.DoneDate.key]?.date?.start?.toDate(),
-        memo = checkNotNull(properties[Property.Memo.key]?.richTexts?.concatText()),
+        doneDate = properties[ShoppingItemProperty.DoneDate.key]?.date?.start?.toDate(),
+        memo = checkNotNull(properties[ShoppingItemProperty.Memo.key]?.richTexts?.concatText()),
         tag = null,
     )
 }
 
 val ShoppingItemPage.relations: List<Relation>
-    get() = checkNotNull(properties[Property.Tag.key]?.relations)
+    get() = checkNotNull(properties[ShoppingItemProperty.Tag.key]?.relations)
 
-fun ShoppingItem.toProperties(): Map<String, PropertyModel> {
+fun ShoppingItem.toProperties(): Map<String, Property> {
     return mutableMapOf(
-        Property.Name.key to PropertyModel(title = name.toRichTextList()),
-        Property.Count.key to PropertyModel(number = count.toLong()),
-        Property.Status.key to PropertyModel(select = Select(status.text)),
-        Property.DoneDate.key to PropertyModel(date = Date(start = doneDate?.toDateString() ?: "")),
-        Property.Memo.key to PropertyModel(richTexts = memo.toRichTextList()),
+        ShoppingItemProperty.Name.key to Property(title = name.toRichTextList()),
+        ShoppingItemProperty.Count.key to Property(number = count.toLong()),
+        ShoppingItemProperty.Status.key to Property(select = Select(status.text)),
+        ShoppingItemProperty.DoneDate.key to Property(date = Date(start = doneDate?.toDateString()
+            ?: "")),
+        ShoppingItemProperty.Memo.key to Property(richTexts = memo.toRichTextList()),
     ).let { map ->
         tag?.let { tag ->
-            map += Property.Tag.key to PropertyModel(relations = listOf(Relation(tag.id.toString())))
+            map += ShoppingItemProperty.Tag.key to Property(relations = listOf(Relation(tag.id.toString())))
         }
         map
     }
 }
 
-private enum class Property(val key: String) {
+private enum class ShoppingItemProperty(val key: String) {
     Name("Name"),
     Count("Count"),
     Status("Status"),
