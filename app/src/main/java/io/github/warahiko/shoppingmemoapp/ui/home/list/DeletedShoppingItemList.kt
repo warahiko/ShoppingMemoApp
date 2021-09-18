6e5d5ad562e39318db46907d20011c2dadbb3d28
go shibata <warahiko@gmail.com>
2021-09-18 23:48:47 +0900

    アイテムが空の場合はその旨を表示

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/list/DeletedShoppingItemList.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/home/list/DeletedShoppingItemList.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import io.github.warahiko.shoppingmemoapp.R
 import io.github.warahiko.shoppingmemoapp.data.model.ShoppingItem
 import io.github.warahiko.shoppingmemoapp.ui.preview.getSampleList
 import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
@@ -21,6 +25,16 @@ fun DeletedShoppingItemList(
     shoppingItems: List<ShoppingItem>,
     modifier: Modifier = Modifier,
 ) {
+    if (shoppingItems.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Text(
+                stringResource(id = R.string.home_list_empty),
+                modifier = Modifier.align(Alignment.Center),
+            )
+        }
+        return
+    }
+
     LazyColumn(
         modifier = modifier
             .padding(8.dp)
