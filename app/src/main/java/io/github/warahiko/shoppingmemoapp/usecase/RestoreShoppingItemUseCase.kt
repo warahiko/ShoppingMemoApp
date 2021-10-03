56b2e70caadf196737e839491fda089c052b99ba
go shibata <warahiko@gmail.com>
2021-10-03 19:33:12 +0900

    レストア用のUseCase 追加

---
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/usecase/RestoreShoppingItemUseCase.kt
@@ -0,0 +1,16 @@
+package io.github.warahiko.shoppingmemoapp.usecase
+
+import io.github.warahiko.shoppingmemoapp.data.model.ShoppingItem
+import io.github.warahiko.shoppingmemoapp.data.model.Status
+import io.github.warahiko.shoppingmemoapp.data.repository.ShoppingListRepository
+import javax.inject.Inject
+
+class RestoreShoppingItemUseCase @Inject constructor(
+    private val shoppingListRepository: ShoppingListRepository,
+) {
+    suspend operator fun invoke(shoppingItem: ShoppingItem) {
+        val newStatus = if (shoppingItem.doneDate == null) Status.NEW else Status.DONE
+        val restored = shoppingItem.copy(status = newStatus)
+        shoppingListRepository.updateShoppingItem(restored)
+    }
+}
