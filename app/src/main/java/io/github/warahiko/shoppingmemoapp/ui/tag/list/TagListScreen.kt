dd0e2a706fddb9be536bb0c9ef038d27c05b7bd6
go shibata <warahiko@gmail.com>
2021-10-09 17:06:13 +0900

    タグ編集画面への遷移追加

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/tag/list/TagListScreen.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/tag/list/TagListScreen.kt
@@ -1,11 +1,19 @@
 package io.github.warahiko.shoppingmemoapp.ui.tag.list
 
+import androidx.compose.foundation.LocalIndication
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Divider
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -17,10 +25,17 @@ import androidx.compose.material.icons.filled.Label
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.accompanist.swiperefresh.SwipeRefresh
@@ -32,6 +47,7 @@ import io.github.warahiko.shoppingmemoapp.ui.ShoppingMemoAppBar
 @Composable
 fun TagListScreen(
     onClickAddButton: () -> Unit = {},
+    onEdit: (tag: Tag) -> Unit,
     viewModel: TagListScreenViewModel = hiltViewModel(),
 ) {
     val tags by viewModel.tags.collectAsState()
@@ -59,6 +75,7 @@ fun TagListScreen(
             tags = tags,
             isRefreshing = isRefreshing,
             onRefresh = viewModel::fetchTags,
+            onEdit = onEdit,
         )
     }
 }
@@ -69,6 +86,7 @@ private fun TagListScreenContent(
     isRefreshing: Boolean,
     modifier: Modifier = Modifier,
     onRefresh: () -> Unit = {},
+    onEdit: (tag: Tag) -> Unit = {},
 ) {
     SwipeRefresh(
         state = rememberSwipeRefreshState(isRefreshing),
@@ -101,11 +119,9 @@ private fun TagListScreenContent(
                     Divider(color = MaterialTheme.colors.onBackground)
                 }
                 itemsIndexed(list, key = { _, item -> item.id }) { index, item ->
-                    Text(
-                        item.name,
-                        modifier = Modifier
-                            .padding(start = 32.dp)
-                            .padding(vertical = 8.dp)
+                    ItemRow(
+                        tag = item,
+                        onEdit = onEdit,
                     )
                     if (index < list.size - 1) {
                         Divider(
@@ -115,6 +131,66 @@ private fun TagListScreenContent(
                         )
                     }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun ItemRow(
+    tag: Tag,
+    onEdit: (tag: Tag) -> Unit = {},
+) {
+    var showOperation by remember { mutableStateOf(false) }
+    var dropdownOffset by remember { mutableStateOf(Offset.Zero) }
+
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .indication(
+                interactionSource = interactionSource,
+                indication = LocalIndication.current,
+            )
+            .pointerInput(tag) {
+                detectTapGestures(
+                    onLongPress = {
+                        showOperation = true
+                        dropdownOffset = it
+                    },
+                    onPress = {
+                        val press = PressInteraction.Press(it)
+                        interactionSource.emit(press)
+                        val result = if (tryAwaitRelease()) {
+                            PressInteraction.Release(press)
+                        } else {
+                            PressInteraction.Cancel(press)
+                        }
+                        interactionSource.emit(result)
+                    }
+                )
+            },
+    ) {
+        Text(
+            tag.name,
+            modifier = Modifier
+                .padding(start = 32.dp)
+                .padding(vertical = 8.dp)
+        )
+        DropdownMenu(
+            expanded = showOperation,
+            onDismissRequest = { showOperation = false },
+            offset = LocalDensity.current.run {
+                DpOffset(dropdownOffset.x.toDp(), 0.dp)
+            },
+        ) {
+            DropdownMenuItem(onClick = { onEdit(tag) }) {
+                Text(stringResource(R.string.tag_list_operation_edit))
+            }
+            Divider()
+            DropdownMenuItem(onClick = { showOperation = false }) {
+                Text(stringResource(R.string.cancel))
             }
         }
     }
