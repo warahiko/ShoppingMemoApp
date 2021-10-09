93ca395b657333b422090d25e90116ac6d2f6fb4
go shibata <warahiko@gmail.com>
2021-10-09 18:13:39 +0900

    色変更

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/theme/Color.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package io.github.warahiko.shoppingmemoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightGreen = Color(0xFF9CCC65)
+val Green = Color(0xFF8BC34A)
+val LightRed = Color(0xFFFF8A65)
