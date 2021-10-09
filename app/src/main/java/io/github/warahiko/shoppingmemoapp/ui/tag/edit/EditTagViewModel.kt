f9dcc06b68adb2696f0236667902c00113d7a844
go shibata <warahiko@gmail.com>
2021-10-09 17:02:05 +0900

    タグ編集用の画面追加

---
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/ui/tag/edit/EditTagViewModel.kt
@@ -0,0 +1,42 @@
+package io.github.warahiko.shoppingmemoapp.ui.tag.edit
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.warahiko.shoppingmemoapp.data.model.Tag
+import io.github.warahiko.shoppingmemoapp.data.repository.TagListRepository
+import io.github.warahiko.shoppingmemoapp.error.LaunchSafe
+import io.github.warahiko.shoppingmemoapp.ui.common.ext.withLoading
+import io.github.warahiko.shoppingmemoapp.usecase.tag.EditTagUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import java.util.UUID
+import javax.inject.Inject
+
+@HiltViewModel
+class EditTagViewModel @Inject constructor(
+    private val tagListRepository: TagListRepository,
+    private val editTagUseCase: EditTagUseCase,
+    launchSafe: LaunchSafe,
+) : ViewModel(), LaunchSafe by launchSafe {
+
+    val types: StateFlow<List<String>> =
+        tagListRepository
+            .types
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(1000), emptyList())
+
+    private val _showProgress = MutableStateFlow(false)
+    val showProgress: StateFlow<Boolean> get() = _showProgress
+
+    fun getTag(id: String): Tag? {
+        return tagListRepository.tagList.value?.singleOrNull {
+            it.id == UUID.fromString(id)
+        }
+    }
+
+    fun editTag(tag: Tag) = viewModelScope.launchSafe {
+        editTagUseCase(tag)
+    }.withLoading(_showProgress)
+}
