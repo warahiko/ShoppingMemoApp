209f7fd179666ee1adacbeb4bbc7135195137f23
go shibata <warahiko@gmail.com>
2021-10-09 19:26:39 +0900

    最近のアプリでのヘッダカラー変更

--- app/src/main/java/io/github/warahiko/shoppingmemoapp/MainActivity.kt
+++ app/src/main/java/io/github/warahiko/shoppingmemoapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package io.github.warahiko.shoppingmemoapp
 
+import android.app.ActivityManager
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.color.MaterialColors
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.warahiko.shoppingmemoapp.ui.ShoppingMemoNavHost
 import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
@@ -11,10 +13,23 @@ import io.github.warahiko.shoppingmemoapp.ui.theme.ShoppingMemoAppTheme
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setRecentAppsAppearance()
+
         setContent {
             ShoppingMemoAppTheme {
                 ShoppingMemoNavHost()
             }
         }
+    }
+
+    private fun setRecentAppsAppearance() {
+        @Suppress("Deprecation")
+        val taskDescription = ActivityManager.TaskDescription(
+            null,
+            null,
+            MaterialColors.getColor(this, R.attr.colorPrimarySurface, 0),
+        )
+        setTaskDescription(taskDescription)
     }
 }
